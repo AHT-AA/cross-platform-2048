44c646cff2229a3d368c3ccab121a819e3342c51
AHT-AA <112618774+AHT-AA@users.noreply.github.com>
2022-10-05 20:10:52 +0300

    2048 builder file for Windows.

--- COMPILE.cmd
+++ COMPILE.cmd
@@ -1,5 +1,5 @@
 rem if your compiler is not on PATH variable set path for compiler. for example
 rem set path=C:\mingw64\bin
 
-@gcc main.c game.c  hscr.c internal.c table.c -o 2048.exe -Wall
+gcc main.c game.c  hscr.c internal.c table.c -o 2048.exe -Wall
 @PAUSE
